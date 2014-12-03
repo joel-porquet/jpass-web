74c15a032d34279f227fa561f49e78386c809d88
Joel Porquet <joel@porquet.org>
2014-12-03 12:12:01 +0100

    Add comments and fix a few syntax potential errors

--- jpass_web.py
+++ jpass_web.py
@@ -4,13 +4,22 @@ import bottle
 from jpass.config import Config
 from jpass.service import Service
 
+## url formatting in templates
 bottle.SimpleTemplate.defaults["get_url"] = bottle.url
 
+## utils
 def get_conf(user):
     conf = Config("/srv/jpass/%s/jpass.conf" % user)
     return conf
 
 
+## routing
+# static files
+@bottle.route("/static/<filepath:path>", name="static")
+def server_static(filepath):
+    return bottle.static_file(filepath, root="static")
+
+# get requests
 @bottle.route("/")
 @bottle.route("/<user>")
 def index(user=None):
@@ -21,26 +30,25 @@ def index(user=None):
     # otherwise try to open the specified conf file
     try:
         conf = get_conf(user)
-    except Exception as e:
-        return bottle.template("An error occured: {{e}}", e = e);
+    except Exception:
+        return "Error: the specified user is unknow or unable to open the configuration file"
+        #return bottle.template("An error occured: {{e}}", e = e);
 
     return bottle.template("index",
             service_list = sorted(conf.linear_dict.keys()))
 
-@bottle.route("/static/<filepath:path>", name="static")
-def server_static(filepath):
-    return bottle.static_file(filepath, root="static")
-
+# post requests
 @bottle.post("/<user>")
 def get_pwd(user=None):
-    if not user:
-        return bottle.template("Unknown user");
+    if user is None:
+        return "Error: cannot post on empty user"
 
     # open the specified conf file
     try:
         conf = get_conf(user)
     except Exception as e:
-        return bottle.template("An error occured: {{e}}", e = e);
+        return "Error: the specified user is unknow or unable to open the configuration file"
+        #return bottle.template("An error occured: {{e}}", e = e);
 
     # get the service from the conf
     service_name = bottle.request.forms.get("service")
@@ -65,6 +73,7 @@ def get_pwd(user=None):
             iden = serv.get_attr("id"),
             comment = serv.get_attr("comment"))
 
+## built-in webserver for development purposes
 if __name__ == "__main__":
     bottle.debug(True)
     bottle.run(reloader=True)
