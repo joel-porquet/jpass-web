86e68aeba449c59bb039167e77ee6a106c29a47b
Joel Porquet <joel@porquet.org>
2014-12-27 16:43:15 +0100

    call flup directly, no need to patch bottle anymore

--- jpass_fcgi.py
+++ jpass_fcgi.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
+import flup.server.fcgi
 import jpass_web
 import os
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
-    jpass_web.bottle.run(server="flup")
+    flup.server.fcgi.WSGIServer(jpass_web.bottle.default_app()).run()
