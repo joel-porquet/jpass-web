00c69baeda001fcaaf61c433eb6aac0a22335fce
Joel Porquet <joel@porquet.org>
2015-10-18 00:02:26 +0200

    switch to flipflop as flup (python3) is difficult to find on archlinux

--- jpass_fcgi.py
+++ jpass_fcgi.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-import flup.server.fcgi
+import flipflop
 import jpass_web
 import os
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
-    flup.server.fcgi.WSGIServer(jpass_web.bottle.default_app()).run()
+    flipflop.WSGIServer(jpass_web.bottle.default_app()).run()
